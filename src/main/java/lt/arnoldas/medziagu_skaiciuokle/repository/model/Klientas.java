401800700cb28ad727790909098beec76f13378e
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-09-04 19:23:15 +0300

    Create klientas and samata entities

--- src/main/java/lt/arnoldas/medziagu_skaiciuokle/repository/model/Klientas.java
+++ src/main/java/lt/arnoldas/medziagu_skaiciuokle/repository/model/Klientas.java
@@ -1,4 +1,35 @@
 package lt.arnoldas.medziagu_skaiciuokle.repository.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "klientai")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Klientas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)     //generuoju random id, klientams
+    @Column(name = "kliento_id")
+    private Integer klientoId;
+
+    @Column(name = "kliento_vardas")
+    private String klietoVardas;
+
+    @Column(name = "kliento_pavarde")
+    private String klientoPavarde;
+
+    @Column(name = "elektroninis_pastas")
+    private String elPastas;
+
+    @Column(name = "salis")
+    private String salis;
+
+    @Column(name = "miestas")
+    private String miestas;
+
 }
--- src/main/java/lt/arnoldas/medziagu_skaiciuokle/repository/model/Samata.java
+++ src/main/java/lt/arnoldas/medziagu_skaiciuokle/repository/model/Samata.java
@@ -1,4 +1,52 @@
 package lt.arnoldas.medziagu_skaiciuokle.repository.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "saskaitos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Samata {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)  //generuoju id didejancius po viena
+    @Column(name = "samatos_id")
+    private Integer samatosId;
+
+    @Column(name = "struktura")
+    private String struktura;
+
+    @Column(name = "strukturos_aukstis")
+    private Double strukturosAukstis;
+
+    @Column(name = "strukturos_ilgis")
+    private Double strukturosIlgis;
+
+    @Column(name = "strukturos_plotis")
+    private Double strukturosPlotis;
+
+    @Column(name = "medziaga")
+    private String medziaga;
+
+    @Column(name = "medziagos_ilgis")
+    private Double medziagosIlgis;
+
+    @Column(name = "medziagos_plotis")
+    private Double medziagosPlotis;
+
+    @Column(name = "medziagos_aukstis")
+    private Double medziagosAukstis;
+
+    @Column(name = "vienetai")
+    private Integer vienetai;
+
+    @Column(name = "plotas")
+    private Double plotas;
+
+    @Column(name = "turis")
+    private Double turis;
 }
