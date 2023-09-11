bf77d4cf9c392574c489a40cad9c9cd84d4116ff
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-09-11 21:05:13 +0300

    Add kvadratura/kubatura calculations

--- src/main/java/lt/arnoldas/medziagu_skaiciuokle/repository/model/Samata.java
+++ src/main/java/lt/arnoldas/medziagu_skaiciuokle/repository/model/Samata.java
@@ -9,7 +9,6 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class Samata {
 
@@ -22,38 +21,100 @@ public class Samata {
     private String struktura;
 
     @Column(name = "strukturos_aukstis")
-    private Double strukturosAukstis;
+    private double strukturosAukstis;
 
     @Column(name = "strukturos_ilgis")
-    private Double strukturosIlgis;
+    private double strukturosIlgis;
 
     @Column(name = "strukturos_plotis")
-    private Double strukturosPlotis;
+    private double strukturosPlotis;
 
     @Column(name = "medziaga")
     private String medziaga;
 
     @Column(name = "medziagos_ilgis")
-    private Double medziagosIlgis;
+    private double medziagosIlgis;
 
     @Column(name = "medziagos_plotis")
-    private Double medziagosPlotis;
+    private double medziagosPlotis;
 
     @Column(name = "medziagos_aukstis")
-    private Double medziagosAukstis;
+    private double medziagosAukstis;
 
     @Column(name = "vienetai")
     private Integer vienetai;
 
-    @Column(name = "plotas")
-    private Double plotas;
+    @Column(name = "kvadratūra")
+    private double kvadratura;
 
-    @Column(name = "turis")
-    private Double turis;
+    @Column(name = "kubatūra")
+    private double kubatura;
 
     @ManyToOne
     @JoinColumn(name = "kliento_id", nullable = false)
     @PrimaryKeyJoinColumn
     @JsonBackReference
     private Klientas klientas;
+
+    public Samata(String struktura, Double strukturosAukstis, Double strukturosIlgis, Double strukturosPlotis, String medziaga, Double medziagosIlgis, Double medziagosPlotis, Double medziagosAukstis, Integer vienetai, Double kvadratura, Double kubatura, Klientas klientas) {
+        this.struktura = struktura;
+        this.strukturosAukstis = strukturosAukstis;
+        this.strukturosIlgis = strukturosIlgis;
+        this.strukturosPlotis = strukturosPlotis;
+        this.medziaga = medziaga;
+        this.medziagosIlgis = medziagosIlgis;
+        this.medziagosPlotis = medziagosPlotis;
+        this.medziagosAukstis = medziagosAukstis;
+        this.vienetai = vienetai;
+        this.kvadratura = kvadratura;
+        this.kubatura = kubatura;
+        this.klientas = klientas;
+    }
+
+    public double calculateStrukturosKvadratura(double strukturosAukstis, double strukturosIlgis){
+        double kvadratura = 0;
+        if(strukturosAukstis != 0 && strukturosIlgis != 0) {
+            kvadratura = strukturosAukstis * strukturosIlgis;
+        }
+        return kvadratura;
+    }
+
+    public double calculateStrukturosKubatura(double strukturosAukstis, double strukturosIlgis, double strukturosPlotis){
+        double kubatura = 0;
+        if(strukturosAukstis != 0 && strukturosIlgis != 0 && strukturosPlotis != 0) {
+            kubatura = strukturosIlgis * strukturosAukstis * strukturosPlotis;
+        }
+        return kubatura;
+    }
+    public double calculateMedziagosKvadratura(double medziagosAukstis, double medziagosIlgis){
+        double kvadratura = 0;
+        if(medziagosAukstis != 0 && medziagosIlgis != 0){
+           kvadratura = medziagosAukstis * medziagosIlgis;
+        }
+        return kvadratura;
+    }
+
+    public double calculateMedziagosKubatura(double medziagosAukstis, double medziagosIlgis, double medziagosPlotis){
+        double kubatura = 0;
+        if(medziagosAukstis != 0 && medziagosIlgis != 0 && medziagosPlotis != 0) {
+            kubatura = medziagosIlgis * medziagosAukstis * medziagosPlotis;
+        }
+        return kubatura;
+    }
+
+    public double calculateVienetai(){
+        double strukturosKvadratura = calculateStrukturosKvadratura(this.strukturosAukstis, this.strukturosIlgis);
+        double medziagosKvadratura = calculateMedziagosKvadratura(this.medziagosAukstis, this.medziagosIlgis);
+        double strukturosKubatura = calculateStrukturosKubatura(this.strukturosAukstis, this.strukturosIlgis, this.strukturosPlotis);
+        double medziagosKubatura = calculateMedziagosKubatura(this.medziagosAukstis, this.medziagosIlgis, this.medziagosPlotis);
+        int vienetai = 0;
+        if(strukturosKvadratura != 0) {
+            vienetai = (int) (strukturosKvadratura / medziagosKvadratura);
+            vienetai = vienetai + (vienetai * 5 / 100);
+        } else {
+            vienetai =(int) (strukturosKubatura / medziagosKubatura);
+            vienetai = vienetai + (vienetai * 5 / 100);
+        }
+        return vienetai;
+    }
 }
