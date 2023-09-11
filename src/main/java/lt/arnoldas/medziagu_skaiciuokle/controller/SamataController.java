eb4bd0fdb05307ed1435f483f22d0b77c46adae3
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-09-11 21:07:56 +0300

    Create samata for Klientas

--- src/main/java/lt/arnoldas/medziagu_skaiciuokle/controller/SamataController.java
+++ src/main/java/lt/arnoldas/medziagu_skaiciuokle/controller/SamataController.java
@@ -2,6 +2,7 @@ package lt.arnoldas.medziagu_skaiciuokle.controller;
 
 import lt.arnoldas.medziagu_skaiciuokle.repository.model.Klientas;
 import lt.arnoldas.medziagu_skaiciuokle.repository.model.Samata;
+import lt.arnoldas.medziagu_skaiciuokle.service.KlientasService;
 import lt.arnoldas.medziagu_skaiciuokle.service.SamataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,21 +18,59 @@ public class SamataController {
     @Autowired
     private SamataService samataService;
 
+    @Autowired
+    private KlientasService klientasService;
+
     // http://localhost:8888/samatos/search
-    @RequestMapping(value = "/search", method = RequestMethod.GET)          // ikelia visas samatas
-    public String getSamatos(Model model){
-        model.addAttribute("key_samata", new Samata());
-        model.addAttribute("key_samatos_list", samataService.getAll());
-        return "samata_list";
+//    @RequestMapping(value = "/search", method = RequestMethod.GET)          // ikelia visas samatas
+//    public String getSamatos(Model model){
+//        model.addAttribute("key_samata", new Samata());
+//        model.addAttribute("key_samatos_list", samataService.getAll());
+//        return "samata_list";
+//    }
+//
+//    @RequestMapping(value = "/search", method = RequestMethod.POST)         // randa samata pagal struktura
+//    public String getSamatos(Model model, @ModelAttribute(value = "key_samata") Samata samata){
+//        model.addAttribute("key_samata", samata);
+//        model.addAttribute(
+//                "key_samatos_list",
+//                samataService.getSamataByStrukturaLike(samata.getStruktura())
+//        );
+//        return "samata_list";
+//    }
+
+    // http://localhost:8888/samatos/create             // sukurti samata !!!!!!!!!!!!keisti urlus
+    @RequestMapping(value = "/create/{klientoId}", method = RequestMethod.GET)      //cia kazkas negerai su ikelimu
+    public String saveSamata(Model model, @PathVariable String klientoId){
+        Klientas klientas = klientasService.getById(klientoId);
+        Samata samata = new Samata();
+        samata.setKlientas(klientas);
+        model.addAttribute("key_samata", samata);
+        return "samata_create";
     }
 
-    @RequestMapping(value = "/search", method = RequestMethod.POST)         // randa samata pagal struktura
-    public String getSamatos(Model model, @ModelAttribute(value = "key_samata") Samata samata){
+    @RequestMapping(value = "/create/{klientoId}", method = RequestMethod.POST)              //paspaudus ant samatos ja editinti  !!!!!!!!!!!!!! keisti urlus
+    public String saveSamata(Model model, @ModelAttribute(value = "key_samata") Samata samata, @PathVariable String klientoId){
+        samataService.save(samata, klientoId);
         model.addAttribute("key_samata", samata);
-        model.addAttribute(
-                "key_samatos_list",
-                samataService.getSamataByStrukturaLike(samata.getStruktura())
-        );
-        return "samata_list";
+        return "samata_create";
+    }
+
+//    @RequestMapping(value = "/create/{klientoId}", method = RequestMethod.GET)
+//    public String saveSamata(Model model){
+//        model.addAttribute("key_samata", new Samata());
+//        return "samata_create";
+//    }
+
+//    @RequestMapping(value = "/create/{klientoId}", method = RequestMethod.POST)              //paspaudus ant samatos ja editinti  !!!!!!!!!!!!!! keisti urlus
+//    public String saveSamata(Model model, @ModelAttribute(value = "key_samata") Samata samata, @PathVariable String klientoId){
+//        int samataId = samataService.save(samata, klientoId);
+//        model.addAttribute("key_samata", samata);
+//        return "samata_create";
+//    }
+
+    @GetMapping(value = "/menu")                // atidaro menu langa
+    private String getMenu(){
+        return "samatos_menu";
     }
 }
--- src/main/java/lt/arnoldas/medziagu_skaiciuokle/service/SamataService.java
+++ src/main/java/lt/arnoldas/medziagu_skaiciuokle/service/SamataService.java
@@ -1,10 +1,13 @@
 package lt.arnoldas.medziagu_skaiciuokle.service;
 
+import lt.arnoldas.medziagu_skaiciuokle.repository.KlientasRepository;
 import lt.arnoldas.medziagu_skaiciuokle.repository.SamataRepository;
+import lt.arnoldas.medziagu_skaiciuokle.repository.model.Klientas;
 import lt.arnoldas.medziagu_skaiciuokle.repository.model.Samata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,11 +16,22 @@ public class SamataService {
     @Autowired
     private SamataRepository samataRepository;
 
+    @Autowired
+    private KlientasRepository klientasRepository;
+
     public List<Samata> getAll() {
         return (List<Samata>) samataRepository.findAll();
     }
 
     public Object getSamataByStrukturaLike(String struktura) {
         return samataRepository.getSamataByStrukturaLike("%" + struktura + "%");
+    }
+
+    public void save(Samata samata, String klientoId){                      // issaugo samata
+        if(klientasRepository.findById(klientoId).isPresent()) {
+            Klientas klientas = klientasRepository.findById(klientoId).get();
+            samata.setKlientas(klientas);
+            samataRepository.save(samata);
+        }
     }
 }
